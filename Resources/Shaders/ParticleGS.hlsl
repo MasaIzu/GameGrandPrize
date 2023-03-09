022970a1e947564d5397784cc790c831eac91a01
takashithiba <k021g1120@g.neec.ac.jp>
2023-03-10 00:16:47 +0900

    正二十面体の描画

--- Resources/Shaders/ParticleGS.hlsl
+++ Resources/Shaders/ParticleGS.hlsl
@@ -1,53 +1,188 @@
 #include "Particle.hlsli"
 
+////センターからのオフセット
+//static const float4 offset_array[8][3] =
+//{
+//	//上
+//	{
+//		float4(0,1,0,0),//左下
+//		float4(1,0,0,0),//左上
+//		float4(0,0,1,0)//右下
+//	},
+//	{
+//		float4(0,1,0,0),//左下
+//		float4(1,0,0,0),//左上
+//		float4(0,0,-1,0)//右下
+//	},
+//	{
+//		float4(0,1,0,0),//左下
+//		float4(-1,0,0,0),//左上
+//		float4(0,0,1,0)//右下
+//	},
+//	{
+//		float4(0,1,0,0),//左下
+//		float4(-1,0,0,0),//左上
+//		float4(0,0,-1,0)//右下
+//	},
+//	//下
+//	{
+//		float4(0,-1,0,0),//左下
+//		float4(1,0,0,0),//左上
+//		float4(0,0,1,0)//右下
+//	},
+//	{
+//		float4(0,-1,0,0),//左下
+//		float4(1,0,0,0),//左上
+//		float4(0,0,-1,0)//右下
+//	},
+//	{
+//		float4(0,-1,0,0),//左下
+//		float4(-1,0,0,0),//左上
+//		float4(0,0,1,0)//右下
+//	},
+//	{
+//		float4(0,-1,0,0),//左下
+//		float4(-1,0,0,0),//左上
+//		float4(0,0,-1,0)//右下
+//	},
+//};
+//
+//[maxvertexcount(8 * 3)]
+//void main(
+//	point VSOutput input[1] : SV_POSITION,
+//	inout TriangleStream<GSOutput>output
+//)
+//{
+//	float4 offset;
+//	GSOutput element;
+//	for (uint j = 0; j < 8; j++)
+//	{
+//		for (uint i = 0; i < 3; i++)
+//		{
+//			//offset = mul(matBillboard, offset_array[i]);
+//			offset = offset_array[j][i] * input[0].scale;
+//			//offset = mul(matBillboard, offset);
+//			element.svpos = input[0].pos + offset;
+//			//element.svpos = input[0].pos + offset_array[i];
+//			element.svpos = mul(mat, element.svpos);
+//			//element.uv = uv_array[i]; 
+//			element.color = input[0].color;
+//			output.Append(element);
+//		}
+//		output.RestartStrip();
+//	}
+//}
+
 //センターからのオフセット
-static const float4 offset_array[8][3] =
+static const float4 offset_array[20][3] =
 {
-	//上
 	{
-		float4(0,1,0,0),//左下
-		float4(1,0,0,0),//左上
-		float4(0,0,1,0)//右下
+		float4(1  ,0, 0.5,0),//左下
+		float4(1  ,0,-0.5,0),//左上
+		float4(0.5,1, 0  ,0)//右下
 	},
 	{
-		float4(0,1,0,0),//左下
-		float4(1,0,0,0),//左上
-		float4(0,0,-1,0)//右下
+		float4(1  , 0, 0.5,0),//左下
+		float4(1  , 0,-0.5,0),//左上
+		float4(0.5,-1, 0  ,0)//右下
 	},
 	{
-		float4(0,1,0,0),//左下
-		float4(-1,0,0,0),//左上
-		float4(0,0,1,0)//右下
+		float4(-1  ,0, 0.5,0),//左下
+		float4(-1  ,0,-0.5,0),//左上
+		float4( 0.5,1, 0  ,0)//右下
 	},
 	{
-		float4(0,1,0,0),//左下
-		float4(-1,0,0,0),//左上
-		float4(0,0,-1,0)//右下
+		float4(-1  , 0, 0.5,0),//左下
+		float4(-1  , 0,-0.5,0),//左上
+		float4(-0.5,-1, 0  ,0)//右下
 	},
-	//下
+
 	{
-		float4(0,-1,0,0),//左下
-		float4(1,0,0,0),//左上
-		float4(0,0,1,0)//右下
-	},
-	{
-		float4(0,-1,0,0),//左下
-		float4(1,0,0,0),//左上
-		float4(0,0,-1,0)//右下
+		float4(0, 0.5,1  ,0),//左下
+		float4(0,-0.5,1  ,0),//左上
+		float4(1, 0  ,0.5,0),//左下
 	},
 	{
-		float4(0,-1,0,0),//左下
-		float4(-1,0,0,0),//左上
-		float4(0,0,1,0)//右下
+		float4( 0, 0.5,1  ,0),//左下
+		float4( 0,-0.5,1  ,0),//左上
+		float4(-1, 0  ,0.5,0),//左下
 	},
 	{
-		float4(0,-1,0,0),//左下
-		float4(-1,0,0,0),//左上
-		float4(0,0,-1,0)//右下
+		float4(0, 0.5,-1  ,0),//左下
+		float4(0,-0.5,-1  ,0),//左上
+		float4(1, 0  ,-0.5,0)//右下
+	},
+	{
+		float4( 0, 0.5,-1  ,0),//左下
+		float4( 0,-0.5,-1  ,0),//左上
+		float4(-1, 0  ,-0.5,0)//右下
+	},
+
+	{
+		float4( 0.5,1  ,0,0),//左下
+		float4(-0.5,1  ,0,0),//左上
+		float4( 0  ,0.5,1,0),//左下
+	},
+	{
+		float4( 0.5,1  , 0,0),//左下
+		float4(-0.5,1  , 0,0),//左上
+		float4(0   ,0.5,-1,0),//左下
+	},
+	{
+		float4( 0.5,-1  ,0,0),//左下
+		float4(-0.5,-1  ,0,0),//左上
+		float4( 0  ,-0.5,1,0),//左下
+	},
+	{
+		float4(0, 0.5,-1  ,0),//左下
+		float4(0,-0.5,-1  ,0),//左上
+		float4(-1, 0  ,-0.5,0)//右下
+	},
+
+	{
+		float4(1  ,0  ,0.5,0),//左下
+		float4(0.5,1  ,0  ,0),//左上
+		float4(0  ,0.5,1  ,0),//左下
+	},
+	{
+		float4(1  , 0  ,-0.5,0),//左下
+		float4(0.5, 1  , 0  ,0),//左上
+		float4(0  ,0.5,-1  ,0),//左下
+	},
+	{
+		float4(-1  ,0  ,0.5,0),//左下
+		float4(-0.5,1  ,0  ,0),//左上
+		float4( 0  ,0.5  ,1,0)//右下
+	},
+	{
+		float4(-1  ,0  ,-0.5,0),//左下
+		float4(-0.5,1  , 0  ,0),//左上
+		float4( 0  ,0.5,-1  ,0),//左下
+	},
+
+	{
+		float4(1  , 0  ,0.5,0),//左下
+		float4(0.5,-1  ,0  ,0),//左上
+		float4(0  ,-0.5,1  ,0),//左下
+	},
+	{
+		float4(1  , 0  ,-0.5,0),//左下
+		float4(0.5,-1  , 0  ,0),//左上
+		float4(0  ,-0.5,-1  ,0),//左下
+	},
+	{
+		float4(-1  , 0  ,0.5,0),//左下
+		float4(-0.5,-1  ,0  ,0),//左上
+		float4(0   ,-0.5,1  ,0),//左下
+	},
+	{
+		float4(-1  , 0  ,-0.5,0),//左下
+		float4(-0.5,-1  , 0  ,0),//左上
+		float4( 0  ,-0.5,-1  ,0),//左下
 	},
 };
 
-[maxvertexcount(8 * 3)]
+[maxvertexcount(20 * 3)]
 void main(
 	point VSOutput input[1] : SV_POSITION,
 	inout TriangleStream<GSOutput>output
@@ -55,7 +190,7 @@ void main(
 {
 	float4 offset;
 	GSOutput element;
-	for (uint j = 0; j < 8; j++)
+	for (uint j = 0; j < 20; j++)
 	{
 		for (uint i = 0; i < 3; i++)
 		{
@@ -72,3 +207,4 @@ void main(
 		output.RestartStrip();
 	}
 }
+
--- UserApplication/Scene/GameScene.cpp
+++ UserApplication/Scene/GameScene.cpp
@@ -21,7 +21,7 @@ void GameScene::Initialize() {
 
 	model_.reset(Model::CreateFromOBJ("UFO", true));
 
-	viewProjection_.eye = { 0,0,5 };
+	viewProjection_.eye = { 0,0,-10 };
 
 	sceneManager_ = SceneManager::GetInstance();
 
@@ -72,19 +72,19 @@ void GameScene::Update() {
 
 	if (input_->TriggerKey(DIK_Z)) {
 		//スペースキーを押していたら
-		for (int i = 0; i < 30; i++)
+		for (int i = 0; i < 1; i++)
 		{
 			const float rnd_life = 290.0f;
 			float life = (float)rand() / RAND_MAX * rnd_life - rnd_life / 2.0f + 10;
 
 			//X,Y,Z全て[-5.0,+5.0f]でランダムに分布
-			const float rnd_pos = 10.0f;
+			const float rnd_pos = 30.0f;
 			Vector3 pos{};
 			pos.x = (float)rand() / RAND_MAX * rnd_pos  - rnd_pos / 2.0f;
 			pos.y = abs((float)rand() / RAND_MAX * rnd_pos - rnd_pos / 2.0f) + 1;
 			pos.z = (float)rand() / RAND_MAX * rnd_pos  - rnd_pos / 2.0f;
 			//追加
-			ParticleMan->InAdd(life, { 0,-3,0 }, pos, 0.1f, 0.1f, { 1,1,1,1 }, { 1,1,1,1 });
+			ParticleMan->OutAdd(life, { 0,0,0 }, {0,100,0}, 10, 10, { 1,1,1,1 }, { 1,1,1,1 });
 		}
 		////スペースキーを押していたら
 		//for (int i = 0; i < 50; i++)

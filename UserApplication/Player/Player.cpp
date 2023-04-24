116acd5747918c666b99b6e739189a62f61db9b4
YuyaHiguchi <k021g1263@g.neec.ac.jp>
2023-04-24 13:09:25 +0900

    走りモーションの直し

--- UserApplication/Player/Player.cpp
+++ UserApplication/Player/Player.cpp
@@ -107,6 +107,9 @@ void Player::Update(const ViewProjection& viewProjection) {
 
 	if (frem < MaxFrem) {
 		frem += 0.013f;
+		if (isPlayMotion) {
+			frem += 0.007;
+		}
 
 	}
 	else {
@@ -119,7 +122,7 @@ void Player::Update(const ViewProjection& viewProjection) {
 		}
 	}
 	if (conboFlag == true) {
-		receptionTime += 0.013f;
+		receptionTime += 0.02f;
 	}
 
 	if (input_->PushKey(DIK_P)) {
@@ -139,7 +142,7 @@ void Player::Update(const ViewProjection& viewProjection) {
 		fremX += -0.01;
 	}
 
-	fbxmodel->ModelAnimation(frem, modelAnim->GetAnimation(static_cast<int>(0)));
+	fbxmodel->ModelAnimation(frem, modelAnim->GetAnimation(static_cast<int>(playerNowMotion)));
 
 
 

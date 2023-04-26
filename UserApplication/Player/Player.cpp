5471d41ada08f2778bed4bcfb9811db20c62da84
takashithiba <k021g1120@g.neec.ac.jp>
2023-04-26 23:37:57 +0900

    パーティクル調整

--- UserApplication/Player/Player.cpp
+++ UserApplication/Player/Player.cpp
@@ -114,18 +114,18 @@ void Player::Update(const ViewProjection& viewProjection) {
 
 		flame++;
 
-		float endflame = 120;
+		float endflame =120;
 
-		float Destruction = easeOutQuin(1.0f - 0.0f) * (flame / endflame);
-		Destruction--;
-		float a = easeOutQuin(1.0f - 0.0f) * (flame / endflame);
+		float Destruction = (0.0f - 1.0f) * easeOutQuin(flame / endflame);
+		Destruction++;
+		float a = (1.0f - 0.0f) * easeOutQuin(flame / endflame);
 
-		float scale = easeOutQuin(0.2f - 0.0f) * (flame / endflame);
+		float scale= (0.2f - 0.0f) * easeOutQuin(flame / endflame);
 
 		FbxModel::ConstBufferPolygonExplosion polygon = fbxmodel->GetPolygonExplosion();
 		fbxmodel->SetPolygonExplosion({ Destruction,scale,polygon._RotationFactor,polygon._PositionFactor });
 		worldTransform_.alpha = a;
-		if (flame >= endflame)
+		if (flame>=endflame)
 		{
 			isAdmission = false;
 			//worldTransform_.alpha = 1;

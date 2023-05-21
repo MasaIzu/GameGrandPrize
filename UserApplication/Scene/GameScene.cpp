86f8ee132574aad8bccc002d988d497ab6e11186
masato izumida <Oioi_0126@outlook.com>
2023-05-22 03:00:09 +0900

    ウルト当たり判定と剣を持たせて完成

--- UserApplication/Scene/GameScene.cpp
+++ UserApplication/Scene/GameScene.cpp
@@ -556,6 +556,8 @@ void GameScene::GameUpdate()
 		gameCamera->Collision();
 		player->SetParticlePos(collisionManager->GetAttackHitWorldPos());
 		boss->bossFish->Damage(2);
+
+		player->AddUltCount(10);
 	}
 
 	if (collisionManager->GetIsWakeEnemyAttackHit()) {
@@ -565,6 +567,8 @@ void GameScene::GameUpdate()
 		minifishes[playerAttackHitNumber].SetAttribute(COLLISION_ATTR_WEAKENEMYS_DEI);
 
 		minifishes[playerAttackHitNumber].OnCollision();
+
+		player->AddUltCount(10);
 	}
 
 
